bf3b4b43498f9da7e94f7d2829e99623f6bc4f24
Gabriel De Luca <pdeluca@unsam.edu.ar>
2024-06-27 20:04:25 +0000

    20240627 - Sincronizar desde mapas_pue:
    cuenca_reconquista/modules/
    cuenca_reconquista/modules/minimap.js

---
+++ pue/mapas/cuenca_reconquista/modules/minimap.js
@@ -0,0 +1,18 @@
+export function minimap (e, z, bounds) {
+    const argenmap = new L.tileLayer('https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png',
+        {
+            minZoom: 1,
+            maxZoom: 20
+        }
+    );
+
+    const map = new L.map('map',
+        {
+            attributionControl: false,
+            center: new L.LatLng(-34.5790, -58.6608),
+            zoom: Z - 1,
+            zoomControl: false,
+            layers: [argenmap]
+        }
+    );
+}
