cdb21c682b08bc60ccf40870eeeb8b43cb95cd7c
Gabriel De Luca <caprieldeluca@gmail.com>
2024-06-28 16:58:25 -0300

    Construir minimapa.

--- pue/mapas/cuenca_reconquista/js/map.js
+++ pue/mapas/cuenca_reconquista/js/map.js
@@ -19,25 +19,60 @@ function init () {
     }
   );
 
-
-
   //=====
   // Créditos
   //=====
 
-  // Elemento del panel
-  const attr = new L.control.attribution(
-    {prefix: ''}
-  ).addTo(map
-  ).getContainer();
-  // Textos
-  const attr_iiia = '<a href="https://iiia.conicet.gov.ar/" title="IIIA | Instituto de Investigación e Ingeniería Ambiental" target="_blank">IIIA</a>';
-  const attr_osm = '<a href="https://www.openstreetmap.org" title="OpenStreetMap" target="_blank">OpenStreetMap</a>';
-  const attr_argenmap = '<a href="https://www.ign.gob.ar/AreaServicios/Argenmap/Introduccion" title="Introducción | Instituto Geográfico Nacional" target="_blank">Argenmap</a>';
-  const attr_leaflet = '<a href="https://leafletjs.com/" title="Leaflet - a JavaScript library for interactive maps" target="_blank">Leaflet</a>';
-  const attr_text = [attr_iiia, attr_argenmap, attr_leaflet, attr_osm].join(' + ');
-  // Definir texto en elemento panel
-  attr.innerHTML = attr_text
+  // Crear el control
+  const attr_ctrl = new L.control.attribution(
+    {
+      prefix: '',
+      position: 'bottomright',
+    }
+  )
+
+  // Agregarlo al mapa
+  attr_ctrl.addTo(map);
+
+  // Construir hiperlink anchors
+  const iiia_attr = '<a href="https://iiia.conicet.gov.ar/" title="IIIA | Instituto de Investigación e Ingeniería Ambiental" target="_blank">IIIA</a>';
+  const osm_attr = '<a href="https://www.openstreetmap.org" title="OpenStreetMap" target="_blank">OpenStreetMap</a>';
+  const argenmap_attr = '<a href="https://www.ign.gob.ar/AreaServicios/Argenmap/Introduccion" title="Introducción | Instituto Geográfico Nacional" target="_blank">Argenmap</a>';
+  const leaflet_attr = '<a href="https://leafletjs.com/" title="Leaflet - a JavaScript library for interactive maps" target="_blank">Leaflet</a>';
+  // Construir el contenido html
+  const attr_contents = [iiia_attr, argenmap_attr, leaflet_attr, osm_attr].join(' + ');
+  // Asignar contenido al elemento del control
+  attr_ctrl.getContainer().innerHTML = attr_contents;
+
+
+  //=====
+  // Control minimapa
+  //=====
+
+  const mm_ctrl = L.control({position: 'bottomright'});
+
+  // onAdd(): Crear y devolver el elemento DOM del panel
+  mm_ctrl.onAdd = function (map) {
+    // crear un elemento div de clase panel_minimapa
+    const panel = L.DomUtil.create('div', 'mm_panel');
+    // deshabilitar propagación de eventos para este elemento
+    L.DomEvent.disableScrollPropagation(panel);
+    L.DomEvent.disableClickPropagation(panel);
+    // crear el contenedor para el minimapa
+    const container = L.DomUtil.create('div', 'mm_container', panel);
+    container.id = "minimap";
+
+    return panel;
+  };
+  // Agregar al mapa
+  mm_ctrl.addTo(map);
+
+  const mm = minimap('minimap', 6);
+
+  //  Dragend (actualizar el minimapa en base a los nuevos bounds del mapa)
+  map.on('zoomend moveend', function (event) {
+    mm.setView(map.getBounds().getCenter(), map.getZoom() - 5 );
+  });
 
 }
 
--- pue/mapas/cuenca_reconquista/js/modules/minimap.js
+++ pue/mapas/cuenca_reconquista/js/modules/minimap.js
@@ -1,4 +1,6 @@
-export function minimap (e, z, bounds) {
+import * as L from 'leaflet';
+
+export function minimap (id, z) {
   const argenmap = new L.tileLayer('https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png',
     {
       minZoom: 1,
@@ -6,13 +8,23 @@ export function minimap (e, z, bounds) {
     }
   );
 
-  const map = new L.map('map',
+  const map = new L.map(id,
     {
       attributionControl: false,
       center: new L.LatLng(-34.5790, -58.6608),
-      zoom: Z - 1,
+      zoom: z,
       zoomControl: false,
       layers: [argenmap]
     }
   );
+  // Deshabilitar eventos sobre el mapa
+  map.boxZoom.disable();
+  map.doubleClickZoom.disable();
+  map.dragging.disable();
+  map.keyboard.disable();
+  map.scrollWheelZoom.disable();
+  map.tapHold.disable();
+  map.touchZoom.disable();
+
+  return map;
 }
